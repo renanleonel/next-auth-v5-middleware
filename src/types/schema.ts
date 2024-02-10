09228e65dd5f83e0beb9d39261c52d4637ae2619
renanleonel <renanleonelpro@gmail.com>
2024-02-10 14:08:28 -0300

    fix: schema

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -4,11 +4,11 @@ export const loginSchema = z.object({
 	email: z
 		.string()
 		.trim()
-		.min(1, { message: 'Insira o email!' })
-		.email({ message: 'Email inválido!' }),
+		.min(1, { message: 'Email required!' })
+		.email({ message: 'Invalid email!' }),
 	password: z
 		.string()
 		.trim()
-		.min(1, { message: 'Insira a senha!' })
-		.min(8, { message: 'Senha deve ter no mínimo 8 caracteres!' }),
+		.min(1, { message: 'Password required!' })
+		.min(8, { message: 'Password must have at least 8 characters!' }),
 });
